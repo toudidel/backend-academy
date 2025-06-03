0ec0004c5b6158b2b96791cd67c331143abfd96a
k.kozlowski <k.kozlowski@reply.com>
2025-06-03 10:14:57 +0200

    Configuration: Get active profiles

--- src/main/java/com/reply/myapp/configuration/service/LocalProfileAwareServiceImpl.java
+++ src/main/java/com/reply/myapp/configuration/service/LocalProfileAwareServiceImpl.java
@@ -1,12 +1,27 @@
 package com.reply.myapp.configuration.service;
 
+import jakarta.annotation.PostConstruct;
+import java.util.Arrays;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 @Profile("local")
-//@Profile({"default", "local"})
+// @Profile("!test")
+// @Profile({"default", "local"})
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class LocalProfileAwareServiceImpl implements ProfileAwareService {
+
+  private final Environment env;
+
+  @PostConstruct
+  public void printActiveProfiles() {
+    log.info(">>>>>>>>>>>> Active profiles: " + Arrays.toString(env.getActiveProfiles()));
+  }
 
   @Override
   public String getSomeString() {
