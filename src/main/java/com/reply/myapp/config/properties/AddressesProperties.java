da76682af40a27467f3445d6b649d7c6150cdf9b
k.kozlowski <k.kozlowski@reply.com>
2025-06-03 10:00:15 +0200

    Configuration: Properties validation

--- src/main/java/com/reply/myapp/config/properties/AddressesProperties.java
+++ src/main/java/com/reply/myapp/config/properties/AddressesProperties.java
@@ -1,14 +1,20 @@
 package com.reply.myapp.config.properties;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 
 @Data
+@Validated
 @Configuration
 @ConfigurationProperties(prefix = "app.addresses")
 public class AddressesProperties {
 
+  @Pattern(regexp = "^\\d{1,3}(\\.\\d{1,3}){3}$", message = "Invalid IP format")
   private String ip;
+
+  @Pattern(regexp = "^\\d{1,3}(\\.\\d{1,3}){3}$", message = "Invalid IP format")
   private String ip2;
 }
