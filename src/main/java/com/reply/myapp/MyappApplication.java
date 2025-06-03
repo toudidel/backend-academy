c70eaaf03d2d0b695c420b433cf7d287dadc82f0
k.kozlowski <k.kozlowski@reply.com>
2025-06-03 13:38:43 +0200

    Database: ManyToOne

--- src/main/java/com/reply/myapp/MyappApplication.java
+++ src/main/java/com/reply/myapp/MyappApplication.java
@@ -1,7 +1,10 @@
 package com.reply.myapp;
 
+import com.reply.myapp.books.model.Author;
 import com.reply.myapp.books.model.Book;
+import com.reply.myapp.books.repository.AuthorRepository;
 import com.reply.myapp.books.repository.BookRepository;
+import java.math.BigDecimal;
 import java.util.List;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -16,25 +19,92 @@ public class MyappApplication {
   }
 
   @Bean
-  CommandLineRunner init(BookRepository bookRepository) {
+  CommandLineRunner init(AuthorRepository authorRepository, BookRepository bookRepository) {
     return args -> {
+      Author author1 =
+          authorRepository.save(Author.builder().firstName("Jan").lastName("Kowalski").build());
+      Author author2 =
+          authorRepository.save(Author.builder().firstName("John").lastName("Doe").build());
+      Author author3 =
+          authorRepository.save(Author.builder().firstName("J.K.").lastName("Rowling").build());
+
       bookRepository.saveAll(
           List.of(
-              Book.builder().title("Encyklopedia PWN").build(),
-              Book.builder().title("Książka telefoniczna").build(),
-              Book.builder().title("Harry Potter cz. 1").build(),
-              Book.builder().title("Harry Potter cz. 2").build(),
-              Book.builder().title("Harry Potter cz. 3").build(),
-              Book.builder().title("Harry Potter cz. 4").build(),
-              Book.builder().title("Harry Potter cz. 5").build(),
-              Book.builder().title("Harry Potter cz. 6").build(),
-              Book.builder().title("Harry Potter cz. 7").build(),
-              Book.builder().title("Harry Potter cz. 8").build(),
-              Book.builder().title("Harry Potter cz. 9").build(),
-              Book.builder().title("Harry Potter cz. 10").build(),
-              Book.builder().title("Harry Potter cz. 11").build(),
-              Book.builder().title("Harry Potter cz. 12").build(),
-              Book.builder().title("Harry Potter cz. 13").build()));
+              Book.builder()
+                  .title("Encyklopedia PWN")
+                  .price(new BigDecimal("100.00"))
+                  .author(author1)
+                  .build(),
+              Book.builder()
+                  .title("Książka telefoniczna")
+                  .price(new BigDecimal("55.50"))
+                  .author(author2)
+                  .build(),
+              Book.builder()
+                  .title("Harry Potter cz. 1")
+                  .price(new BigDecimal("39.99"))
+                  .author(author3)
+                  .build(),
+              Book.builder()
+                  .title("Harry Potter cz. 2")
+                  .price(new BigDecimal("39.99"))
+                  .author(author3)
+                  .build(),
+              Book.builder()
+                  .title("Harry Potter cz. 3")
+                  .price(new BigDecimal("39.99"))
+                  .author(author3)
+                  .build(),
+              Book.builder()
+                  .title("Harry Potter cz. 4")
+                  .price(new BigDecimal("39.99"))
+                  .author(author3)
+                  .build(),
+              Book.builder()
+                  .title("Harry Potter cz. 5")
+                  .price(new BigDecimal("39.99"))
+                  .author(author3)
+                  .build(),
+              Book.builder()
+                  .title("Harry Potter cz. 6")
+                  .price(new BigDecimal("39.99"))
+                  .author(author3)
+                  .build(),
+              Book.builder()
+                  .title("Harry Potter cz. 7")
+                  .price(new BigDecimal("39.99"))
+                  .author(author3)
+                  .build(),
+              Book.builder()
+                  .title("Harry Potter cz. 8")
+                  .price(new BigDecimal("39.99"))
+                  .author(author3)
+                  .build(),
+              Book.builder()
+                  .title("Harry Potter cz. 9")
+                  .price(new BigDecimal("39.99"))
+                  .author(author3)
+                  .build(),
+              Book.builder()
+                  .title("Harry Potter cz. 10")
+                  .price(new BigDecimal("49.99"))
+                  .author(author3)
+                  .build(),
+              Book.builder()
+                  .title("Harry Potter cz. 11")
+                  .price(new BigDecimal("49.99"))
+                  .author(author3)
+                  .build(),
+              Book.builder()
+                  .title("Harry Potter cz. 12")
+                  .price(new BigDecimal("49.99"))
+                  .author(author3)
+                  .build(),
+              Book.builder()
+                  .title("Harry Potter cz. 13")
+                  .price(new BigDecimal("49.99"))
+                  .author(author3)
+                  .build()));
     };
   }
 }
--- src/main/java/com/reply/myapp/books/model/Author.java
+++ src/main/java/com/reply/myapp/books/model/Author.java
@@ -3,30 +3,21 @@ package com.reply.myapp.books.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-/**
- * If you do not provide a default constructor, you will get an error: No default constructor for
- * entity 'com.reply.myapp.books.model.Book'
- */
 @Entity
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Book {
+public class Author {
 
   @Id @GeneratedValue private Long id;
 
-  private String title;
+  private String firstName;
 
-  private BigDecimal price;
-
-  @ManyToOne
-  private Author author;
+  private String lastName;
 }
