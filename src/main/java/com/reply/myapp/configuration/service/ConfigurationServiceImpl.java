da143e039df43db66832f6f25e14c2fac39d74bc
k.kozlowski <k.kozlowski@reply.com>
2025-06-03 09:18:01 +0200

    Configuration: Logger

--- src/main/java/com/reply/myapp/configuration/service/ConfigurationServiceImpl.java
+++ src/main/java/com/reply/myapp/configuration/service/ConfigurationServiceImpl.java
@@ -1,16 +1,22 @@
 package com.reply.myapp.configuration.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ConfigurationServiceImpl implements ConfigurationService {
 
+  private static final Logger logger = LoggerFactory.getLogger(ConfigurationServiceImpl.class);
+
   @Value("${app.some.property}")
   private String someProperty;
 
   @Override
   public String getSomeProperty() {
+    logger.info("Getting some property");
+    logger.debug("Some property is: {}", someProperty);
     return someProperty;
   }
 }
