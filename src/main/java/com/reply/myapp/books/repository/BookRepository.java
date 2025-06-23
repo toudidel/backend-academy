6f6f1d9cb4e753c8eec24e127a40794db1d59d0f
k.kozlowski <k.kozlowski@reply.com>
2025-06-23 12:33:54 +0200

    Database: N+1 Problem - one possible solution

--- src/main/java/com/reply/myapp/books/repository/BookRepository.java
+++ src/main/java/com/reply/myapp/books/repository/BookRepository.java
@@ -1,13 +1,18 @@
 package com.reply.myapp.books.repository;
 
 import com.reply.myapp.books.model.Book;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /** Spring Data JPA: https://docs.spring.io/spring-data/jpa/reference/jpa/query-methods.html */
 // @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
   Page<Book> findByTitleStartsWithIgnoreCase(Pageable pageable, String title);
+
+  @Query("SELECT b FROM Book b JOIN FETCH b.author")
+  List<Book> findAllBooksWithAuthors();
 }
